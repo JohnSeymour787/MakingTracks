cc71d73d6d0aa7a90b8c14d67e452ffab0626106
JohnSeymour787 <john.seymour787@gmail.com>
2020-05-15 19:16:54 -0700

    Initial Commit, added APISupport class

---
+++ MakingTracks/MakingTracks/ObjectiveC/PTVAPISupportClass.h
@@ -0,0 +1,13 @@
+//
+//  PTVAPISupportClass.h
+//  MakingTracks
+//
+//  Created by user169372 on 5/15/20.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+#ifndef PTVAPISupportClass_h
+#define PTVAPISupportClass_h
+
+
+#endif /* PTVAPISupportClass_h */
